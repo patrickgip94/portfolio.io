c29bb636e14487f57eeaa453eb58329379ac85e2
unknown <gippatrick@gmail.com>
2023-05-03 17:44:13 -0700

    Added logo for Head component

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,14 @@ import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>☕ Patrick&apos;s Portfolio</title>
-        <link rel="icon" href="/" />
+        <title>Patrick&apos;s Portfolio</title>
+        <link rel="icon" href="https://i.imgur.com/83VDPFh.png" />
       </Head>
       <div className="relative z-0">
         <div>
